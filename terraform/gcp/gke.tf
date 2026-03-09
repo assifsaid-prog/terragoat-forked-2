12576cc328752b5d86bdba5204deae17a2e57969
Assif <said@mac-1.home>
2026-03-09 14:41:06 +0100

    test

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -133,6 +133,7 @@ resource "google_container_node_pool" "custom_node_pool12" {
   location = var.region
 
   # nosymbiotic: TF-0126 -fp
+  # nosymbiotic: TF-0331 -fp
   node_config {
     image_type = "Ubuntu"
   }
