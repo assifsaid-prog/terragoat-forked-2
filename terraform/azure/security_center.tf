4f09aa404655e686a4825e18fcbd87620b4e2f12
Assif <said@mac-1.home>
2026-04-27 14:33:17 +0200

    test

--- terraform/azure/security_center.tf
+++ terraform/azure/security_center.tf
@@ -3,6 +3,7 @@ resource azurerm_security_center_subscription_pricing "pricing" {
 }
 
 resource azurerm_security_center_contact "contact" {
+  # nosymbiotic: TF-0503 -fp
   alert_notifications = false
   alerts_to_admins    = false
   email               = "some@email.com"
