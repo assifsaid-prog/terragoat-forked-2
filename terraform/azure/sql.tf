609a3340c46550b5f92da4a2e7796a5decff04d8
Assif <said@mac.home>
2026-05-27 17:03:11 +0200

    test

--- terraform/azure/sql.tf
+++ terraform/azure/sql.tf
@@ -102,6 +102,13 @@ resource "azurerm_postgresql_configuration" "thrtottling_config" {
   value               = "off"
 }
 
+resource "azurerm_postgresql_configuration" "thrtottling_config2" {
+  name                = "connection_throttling"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_postgresql_server.example.name
+  value               = "off"
+}
+
 resource "azurerm_postgresql_configuration" "example" {
   name                = "log_checkpoints"
   resource_group_name = azurerm_resource_group.example.name
