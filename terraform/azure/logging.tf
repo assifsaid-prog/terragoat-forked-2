5d8e7c538a4e93f854aec07318d246efea5a662e
Assif <said@mac.home>
2026-05-29 11:39:05 +0200

    test

--- terraform/azure/logging.tf
+++ terraform/azure/logging.tf
@@ -8,3 +8,13 @@ resource azurerm_monitor_log_profile "logging_profile" {
     days    = 30
   }
 }
+resource azurerm_monitor_log_profile "logging_profile2" {
+  storage_account_id = azurerm_storage_account.example.id
+  categories         = ["Action"]
+  locations          = [var.location]
+  name               = "terragoat-${var.environment}"
+  retention_policy {
+    enabled = true
+    days    = 30
+  }
+}
